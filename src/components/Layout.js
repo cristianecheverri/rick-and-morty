675cc3bd364d56674fa51fcf1c9e637fcb8e2855
Cristian Camilo Echeverri <crisechevrri@gmail.com>
2024-11-12 20:44:30 -0500

    Se hacen cambio de nombre de link

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -12,7 +12,7 @@ const Layout = () => {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/prueba">Prueba</a>
+                                <a className="nav-link active" aria-current="page" href="/prueba">Información</a>
                             </li>
                         </ul>
                     </div>
